4eb74457457c12828a17a70ae7a8cbd25636a9e7
Tom Popovich <tpopovich@lunchonlinux.com>
2025-12-02 10:39:43 -0500

    Adding sample ParameterSet and Pick-List powershell arg examples

---
+++ demos/examples/01__powershell_func_params/sample_parameterset_sample.ps1
@@ -0,0 +1,93 @@
+﻿echo "We defined a powershell function MySample-Parameterset"
+
+function MySample-Parameterset
+{
+<#
+  .SYNOPSIS
+     Short example to show "ParameterSets" that powershell supports, here -cd |vs| -url
+     
+  .DESCRIPTION
+     Detailed, powershell will pick the flags/options based on mysample-parameterset is given a switch
+     a user gives a -cd |vs| -url.  As powershell needs some "distinct-ness" to select which ParameterSet to use...
+
+    It Will define SYNTAX:
+    _____________________
+
+    mysample-parameterset [-url] [-Jump] <String> [<CommonParameters>]
+    ... -JUMP will pick a jump from several FIXED choices
+
+    mysample-parameterset [-cd] -Dir <String> [-Pushd] [<CommonParameters>]
+    _____________________
+
+  .EXAMPLE
+     First example, uses -url then it will offer the other flags appropriate for -cd ParameterSet
+     mysample-parameterset -url ...
+  .EXAMPLE
+     Second example, uses -cd then it will offer the other flags appropriate for -cd ParameterSet
+     mysample-parameterset -cd ...
+  #>
+
+  [CmdletBinding(DefaultParameterSetName='ParameterSetURL')]
+  param
+  (
+    # Parameter -url is a lead flag to select ParameterSetURL
+      [Parameter(ParameterSetName='ParameterSetURL', Mandatory=$false)]
+      [switch] $url,
+    
+    # Parameter -url will use -Jump to pick a jump from several FIXED choices
+      [Parameter(ParameterSetName='ParameterSetURL', Position=0, Mandatory=$true)]
+      [ValidateSet('accountability','benefits')]
+      [string] $Jump,
+  
+    # Parameter description
+      [Parameter(ParameterSetName='ParameterSetPushdDir', Mandatory=$false)]
+      [switch]
+      $cd,
+  
+    # Parameter -Dir [in ParameterSetPushdDir] selects a pick-list set of directories
+      [Parameter(ParameterSetName='ParameterSetPushdDir', Mandatory=$true)]
+      [ValidateSet('one_drive','home','project_1')]
+      [string] $Dir,
+  
+    # Parameter -Pushd, if specified means we need to "pushd" and not "cd" to the directory
+      [Parameter(ParameterSetName='ParameterSetPushdDir', Mandatory=$false)]
+      [switch] $Pushd
+  )
+
+  # This is to map the pick-list Parameter value to some sample folder
+  # also note how we use env variables in double quote strings, and assume
+  # you userid is "fred" there are env variables on windows of:
+  # USERNAME                       fred                                                                                
+  # USERPROFILE                    C:\Users\fred
+  $DIRS = @{
+    'one_drive' = "$( $env:USERPROFILE  )\onedrive"
+    'home' = "$( $env:USERPROFILE  )"
+    'project_1' = "/tmp/project_1"
+  }
+
+  # This following sample code gets executed when the function is called with -cd or -url
+  # and we simply use parameters to cd/pushd to sample folders and for -url do nothing special
+  $chosenParameterSet = $PSCmdlet.ParameterSetName
+  switch($chosenParameterSet)
+  {
+    'ParameterSetURL'    {
+                echo 'User has chosen ParameterSetURL' 
+                
+              } 
+    'ParameterSetPushdDir'    { 
+                echo 'User has chosen ParameterSetPushdDir, has optional switch -Pushd'
+                
+                [string]$the_dir = $DIRS[$Dir]
+
+                echo "... going to dir: $the_dir"
+                if ($Pushd) {
+                  pushd $the_dir  ## user wants to "pushd" rather than just "cd"
+                } else {
+                  cd $the_dir
+                }
+           } 
+  }
+
+
+
+}
---
+++ demos/examples/01__powershell_func_params/sample_simple_pick_list.ps1
@@ -0,0 +1,58 @@
+﻿echo "We defined a powershell function Get-EmpPickList"
+
+function Get-EmpPickList
+{
+<#
+  .SYNOPSIS
+     Get-EmpPickList in a Short description shows "PickList" being mandatory, flat -ID being optional
+  .DESCRIPTION
+     Detailed: goal is to define this syntax:
+    _____________________
+     Get-EmpPickList  [[-ID] <int>] -Position <'PM','Manager','Software','QA'>]
+    _____________________
+
+
+     Get-EmpPickList in short description shows a sample "PickList" being mandatory,
+     and has another optional argument called -ID
+     
+     
+  .EXAMPLE
+     First example w/o options will prompt for the mandatory ones:
+     Get-EmpPickList
+
+     PS C:\> Get-EmpPickList 
+	cmdlet Get-EmpPickList at command pipeline position 1
+	Supply values for the following parameters:
+	Position: QA
+
+    As seen, above, powershell prompted for only the "-Position" flag
+    since that param was Mandatory.
+
+  .EXAMPLE
+     Second example gives -Position as a lead flag and when run we get a pick-list
+     Get-EmpPickList -Position (control space here shows all pick list)
+  #>
+
+  [CmdletBinding()]
+  param
+  (
+    # Parameter -ID is OPTIONAL gives employee id , if known
+    # NOTE this is a whole-number integer type like 101
+    [Parameter(Position=0, Mandatory=$false)]
+    [int] $ID,
+
+    # Parameter -Position in selects a pick-list set of job titles
+    # and we validate that the user enters just values in the pick-list
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('PM','Manager','Software','QA')]
+    [string] $Position
+  )
+
+
+  # TODO: you can place the real custom function code here to run,
+  #       we just echo back the value of -Position that was selected.
+
+  echo $Position
+
+
+}
